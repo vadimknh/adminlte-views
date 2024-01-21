90b7eba24923f446a4848ee30407de32da7a9855
Vadim1108 <kononyhin.vadim1@gmail.com>
2024-01-21 16:37:06 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# AdminLTE 3 starter for Laravel
+
+Used [AdminLTE](https://github.com/ColorlibHQ/AdminLTE) starter template, sliced and prepared for laravel blade.
+CSS an JS connected via cdn. Check layouts/main.blade.php
+
+## Use
+
+Copy admin/ folder to resources/views/
+
+Set up roots and controllers. Continue dev
---
+++ admin/layouts/main.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <!-- Theme style -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/css/adminlte.min.css"
+          integrity="sha512-IuO+tczf4J43RzbCMEFggCWW5JuX78IrCJRFFBoQEXNvGI6gkUw4OjuwMidiS4Lm9Q2lILzpJwZuMWuSEeT9UQ=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+    <!-- Bootstrap css and scripts build by vite. If need cdn replace this line -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+    @include('admin.includes.navbar')
+    @include('admin.includes.sidebar')
+    @yield('content')
+    @include('admin.includes.footer')
+</div>
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- AdminLTE App -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/adminlte.min.js"
+        integrity="sha512-KBeR1NhClUySj9xBB0+KRqYLPkM6VvXiiWaSz/8LCQNdRpUm38SWUrj0ccNDNSkwCD9qPA4KobLliG26yPppJA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
