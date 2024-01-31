bfdce91688f93cb47a25a5ab543400426625c6ac
Vadim K <kononyhin.vadim1@gmail.com>
2024-01-31 21:31:34 +0200

    Restore preloader

--- admin/layouts/main.blade.php
+++ admin/layouts/main.blade.php
@@ -15,6 +15,10 @@
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="{{ asset('AdminLTE-3.2.0/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
+    </div>
     @include('adminlte-laravel-starter-views.admin.includes.navbar')
     @include('adminlte-laravel-starter-views.admin.includes.sidebar')
     @yield('content')
