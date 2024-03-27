4584bbd891413a82e4edea802d96363328c6f51a
Vadim <63558184+vadimknh@users.noreply.github.com>
2024-03-27 17:11:36 +0300

    Update logo

--- includes/admin/sidebar.blade.php
+++ includes/admin/sidebar.blade.php
@@ -1,12 +1,11 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <span class="brand-text font-weight-light brand-link">AdminLTE 3</span>
-{{--    <a href="index3.html" class="brand-link">--}}
-{{--        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"--}}
-{{--             style="opacity: .8">--}}
-{{--        <span class="brand-text font-weight-light">AdminLTE 3</span>--}}
-{{--    </a>--}}
+    <a href="index3.html" class="brand-link">
+       <img src="{{ asset('AdminLTE-3.2.0/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+       <span class="brand-text font-weight-light">AdminLTE 3</span>
+   </a>
 
     <!-- Sidebar -->
     <div class="sidebar mt-3">
